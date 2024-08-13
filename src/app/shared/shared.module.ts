8fb00ed143ca38b3070ade55be4b3dcc05c3c0a1
Ladzi031 <ladziladzani@gmail.com>
2024-08-13 12:56:44 +0200

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { PriceDetailsComponent } from './price-details/price-details.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    CartItemComponent,
+    PriceDetailsComponent,
+    SidePanelComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
