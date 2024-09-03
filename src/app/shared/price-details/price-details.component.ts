8d535988f34640bd4deff2cbdbe00da7efc586b0
Ladzi031 <ladziladzani@gmail.com>
2024-09-03 12:50:13 +0200

    full impl of cart-component

--- src/app/shared/price-details/price-details.component.ts
+++ src/app/shared/price-details/price-details.component.ts
@@ -18,6 +18,17 @@ export class PriceDetailsComponent implements OnInit {
   public ngOnInit(): void {
     this.cartItems = this.cartService.getAllCartItems();
     this.getBillingDetails();
+    this.cartService.cartSubject.subscribe({
+      next: (items: any) => {
+        this.cartItems = items;
+        this.getBillingDetails();
+        /* 
+          whenever the is a state change in the cartProducts, it will trigger the observable to emit the new state change to all subscribers (1 to 1) and this code here will run, in turn will trigger the getBillingDetails() method in this component, the change will be reflected in template
+
+          There is surely a better way of doing things, we could heavily make use of Subjects and Observable to achieve the same behaviour in a much cleaner and easier to understand way
+        */
+      }
+    })
   }
 
   public getBillingDetails() {
